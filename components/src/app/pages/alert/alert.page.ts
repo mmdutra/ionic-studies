a5ee391269a054b31bd1ad2501145777fa528986
Mateus Morais <mmdutra14@gmail.com>
2020-08-09 02:06:36 -0300

    conhecendo mais os alerts e ion-items

--- components/src/app/pages/alert/alert.page.ts
+++ components/src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  protected title: string = 'Alert page';
+
+  constructor(public alertController: AlertController) { }
 
   ngOnInit() {
+  }
+  
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'Nome'
+        }
+      ],
+      buttons: [{
+        text: 'Cancel',
+        role: 'text-danger',
+        handler: (data) => {
+          console.log(`Confirm cancel ${data}`);
+        }
+      }, {
+        text: 'Okay',
+        handler: (data) => {
+          this.title = data.name;
+        }
+      }]
+    });
+
+    await alert.present();
+  }
+
+  favorite(): void {
+    alert("favorite");
+  }
+
+  share(): void {
+    alert("share");
+  }
+
+  unread(): void {
+    alert("unread");
   }
 
 }
